86fb73b626259837b75c489d78e66431eba2952d
snery <vano7star@gmail.com>
2026-09-14 03:08:30 +0300

    added functions for check input, fixed menu and main

--- pipe_calc/pipe_calc/pipe_calc.cpp
+++ pipe_calc/pipe_calc/pipe_calc.cpp
@@ -9,6 +9,7 @@ struct Pipe
     string name_p;
     int diam;
     double length;
+    bool in_repair;
 };
 
 struct CS
@@ -19,10 +20,91 @@ struct CS
     string class_cs;
 };
 
-int main()
+bool pipe_exist = false;
+bool cs_exist = false;
+Pipe pipe;
+CS cs;
+
+// functions for check input
+
+int int_input(string message)
 {
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
+    int input;
+    while (true)
+    {
+        cout << message;
+        cin >> input;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1111, '\n');
+            cout << "The value entered was not a integer number. Enter a valid value.\n";
+        }
+        else
+        {
+            cin.ignore(1111, '\n');
+            return input;
+        }
+    }
+}
+
+double double_input(string message)
+{
+    double input;
+    while (true)
+    {
+        cout << message;
+        cin >> input;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1111, '\n');
+            cout << "The value entered was not a number. Enter a valid value.\n";
+        }
+        else
+        {
+            cin.ignore(1111, '\n');
+            return input;
+        }
+    }
+}
+
+bool bool_input(string message) 
+
+{
+    string input;
+    while (true)
+    {
+        cout << message << "(y/n)";
+        getline(cin, input);
+        if (input == "y") return true;
+        if (input == "n") return false;
+        cout << "Enter y or n";
+    }
+}
+
+string string_input(string message)
+{
+    string input;
+    while (true)
+    {
+        cout << message;
+        getline(cin, input);
+        if (input == "")
+        {
+            cout << "Cannot be empty. Enter something.";
+        }
+        else
+        {
+            return input;
+        }
+    }
+}
+
+// menu
+
+void menu()
+{
     cout << " 1. Add pipe\n";
     cout << " 2. Add cs\n";
     cout << " 3. View all objects\n";
@@ -31,4 +113,10 @@ int main()
     cout << " 6. Save\n";
     cout << " 7. Load\n";
     cout << " 0. Exit\n";
+}
+
+int main()
+{
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 }
